f824dad3888010e35ec7b8f52bfa6260efa8ba4c
esavary <elodie.savary@epfl.ch>
2018-03-23 15:27:49 +0100

    Update geomtimedelai.py

--- geomtimedelai.py
+++ geomtimedelai.py
@@ -4,15 +4,16 @@ import matplotlib.pyplot as plt
 axis_font = {'fontname':'Arial', 'size':'18'}
 
 
-def rt(t):
+def rt(t): #radius of the supernova function of t
     rt=10**(-5)+t/30.
     return rt
 def flux(t):
     magnitude = 2 * 10 ** (-6) * (t - 20) ** 4 - 0.00022 * (t - 20) ** 3 + 0.0077 * (t - 20) ** 2 + 0.0025 * (t - 20) - 19.5
-    f = 10 **(magnitude / (-2.5)) * 4000
-    return f
+    f = 10 **(magnitude / (-2.5)) * 4000 # equation to transform magnitude into flux, 4000~= zero point for blue 
+    return 
 
-def normint(gridstep,xmin,xmax,ymin,ymax,t):
+#normalisation factor, the integral is computed by taking the sum on all pixels in the chosen grid
+def normint(gridstep,xmin,xmax,ymin,ymax,t,z):
     inttot=[]
     for x in np.arange(xmin,xmax,gridstep):
         for y in np.arange(ymin,ymax,gridstep):
@@ -22,7 +23,8 @@ def normint(gridstep,xmin,xmax,ymin,ymax,t):
     tot=np.array(inttot)
     return np.sum(tot)
 
-def nonormmeandelaiint(gridstep,xmin,xmax,ymin,ymax,t):
+#non normalized mean delta t, the integral is computed by taking the sum on all pixels in the chosen grid
+def nonormmeandelaiint(gridstep,xmin,xmax,ymin,ymax,t,z):
     inttot=[]
     for x in np.arange(xmin,xmax,gridstep):
         for y in np.arange(ymin,ymax,gridstep):
@@ -30,25 +32,26 @@ def nonormmeandelaiint(gridstep,xmin,xmax,ymin,ymax,t):
                 delai=-np.sqrt(rt(t)**2-x**2-y**2)
                 inttot.append(flux(t+delai)*delai/(pi*2*rt(t+delai)**2))
     tot=np.array(inttot)
-    return np.sum(tot)
+    return (1+z)*np.sum(tot)
 
-def meandelai(gridstep,xmin,xmax,ymin,ymax,t):
-
-    meandelai=nonormmeandelaiint(gridstep,xmin,xmax,ymin,ymax,t)*1./normint(gridstep,xmin,xmax,ymin,ymax,t)
+def meandelai(gridstep,xmin,xmax,ymin,ymax,t,z):
+    #mean delai normalized 
+    meandelai=nonormmeandelaiint(gridstep,xmin,xmax,ymin,ymax,t,z)*1./normint(gridstep,xmin,xmax,ymin,ymax,t,z)
     return meandelai
 
 ####display
-def plotmeandt(gridstep,xmin,xmax,ymin,ymax,timetab):
+def plotmeandt(gridstep,xmin,xmax,ymin,ymax,timetab,z):#plot the mean time delai for an array of time
     mdt=[]
     for t in timetab:
-        mdt.append(meandelai(gridstep,xmin,xmax,ymin,ymax,t))
+        mdt.append(meandelai(gridstep,xmin,xmax,ymin,ymax,t,z))
 
     plt.plot(timetab,mdt)
     plt.tick_params(labelsize=16)
     plt.xlabel(' t [ld]',** axis_font)
     plt.ylabel('<$\delta t)$> [ld]',** axis_font)
     plt.show()
-def drawcolormap(gridstep,xmin,xmax,ymin,ymax,t): #plot the map +return the array
+def drawcolormap(gridstep,xmin,xmax,ymin,ymax,t): 
+    #plot the delay map at a given t +return the array containing the values.
     dimmapx=int((xmax-xmin)*1./gridstep)
     dimmapy = int((ymax - ymin) * 1. / gridstep)
     delaimap = np.zeros((dimmapx, dimmapy))
@@ -67,13 +70,16 @@ def drawcolormap(gridstep,xmin,xmax,ymin,ymax,t): #plot the map +return the arra
 
 
 if __name__ == '__main__':
-
+    
+    c=1.
+    z=1.
+    # min and max coordinates of the grid:
     xmax=5
     ymax=5
     xmin=-5
     ymin=-5
     timetab=np.arange(0,40,1)
     gridstep=0.01
-    plotmeandt(gridstep,xmin,xmax,ymin,ymax,timetab)
+    plotmeandt(gridstep,xmin,xmax,ymin,ymax,timetab,z)
     delaimap=drawcolormap(gridstep,xmin,xmax,ymin,ymax,timetab[30])
 
